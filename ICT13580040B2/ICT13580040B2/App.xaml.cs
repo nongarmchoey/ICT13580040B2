08cf31536644701f5f4b785939815476e200bd31
Theerapat <kingkong_armza@hotmail.com>
2017-12-19 23:15:56 +0700

    project2
    finalproject

---
+++ ICT13580040B2/ICT13580040B2/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ICT13580040B2.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ICT13580040B2
+{
+    public partial class App : Application
+    {
+        public static DbHelper DbHelper { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        public App(string dbPath)
+        {
+            InitializeComponent();
+
+            DbHelper = new DbHelper(dbPath);
+
+            MainPage = new NavigationPage(new ICT13580040B2.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ICT13580040B2/ICT13580040B2/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ICT13580040B2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ICT13580040B2
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            newButton.Clicked += NewButton_Clicked;
+        }
+
+        protected override void OnAppearing()
+        {
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            productListView.ItemsSource = App.DbHelper.GetProducts();
+        }
+
+        void NewButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new ProductNewPage());
+        }
+
+        void Edit_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as MenuItem;
+            var product = button.CommandParameter as Product;
+            Navigation.PushModalAsync(new ProductNewPage(product));
+        }
+
+        async void Delete_Clicked(object sender, EventArgs e)
+        {
+            var isOk = await DisplayAlert("ยืนยัน", "คุณต้องการลบใช่หรือไม่", "ใช่", "ไม่ใช่");
+
+            if (isOk)
+            {
+                var button = sender as MenuItem;
+                var product = button.CommandParameter as Product;
+                App.DbHelper.DeleteProduct(product);
+
+                await DisplayAlert("ลบสำเร็จ", "ลบข้อมูลสินค้าเรียบร้อย", "ตกลง");
+                LoadData();
+            }
+        }
+    }
+}
